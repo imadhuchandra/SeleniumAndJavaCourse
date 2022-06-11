78689c74e549171edfdddef96657148c710c5974
Madhu Chandra <maddurimadhuchandra2@gmail.com>
2022-06-11 11:32:19 +0530

    Create loops.java

---
+++ src/main/java/loops.java
@@ -0,0 +1,22 @@
+public class loops {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+
+        }
+
+        int i = 0;
+
+        while (i<=5) {
+            System.out.println(i);
+            i++;
+        }
+
+        do{
+            System.err.println(i);
+            i++;
+        }while(false);
+
+    }
+}
